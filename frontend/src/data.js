b6d211e230f73c8896ea0fb71240785c4078d6a9
anibal <anibals2012@gmail.com>
2023-05-16 12:38:23 -0500

    agregando routing

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Aceite Vegetal PRIMOR Premium Botella 900ml',
+      slug: 'aceite vegetal',
+      category: 'abarrotes',
+      image: '/images/aceite.jpeg', //679px x 829px
+      price: '12.10',
+      countInStock: 10,
+      brand: 'Primor',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Producto de Alta Calidad',
+    },
+    {
+      name: 'Arroz Extra PAISANA Bolsa 5Kg',
+      slug: 'arroz extra',
+      category: 'abarrotes',
+      image: '/images/arroz.jpeg',
+      price: '20.90',
+      countInStock: 10,
+      brand: 'Paisana',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Producto de Alta Calidad',
+    },
+    {
+      name: 'Fideos Tallarín NICOLINI Bolsa 950g',
+      slug: 'Fideos Tallarín',
+      category: 'abarrotes',
+      image: '/images/fideo.jpeg',
+      price: '5.29',
+      countInStock: 10,
+      brand: 'Nicolini',
+      rating: 3,
+      numReviews: 10,
+      description: 'Producto de Alta Calidad',
+    },
+  ],
+};
+
+export default data;
